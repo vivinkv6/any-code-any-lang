03f9395f1504726a006a36a2a036d5f8ec2a0f8c
vivinkv6 <vivinkv0@gmail.com>
2023-08-18 06:53:44 +0530

    factorial of N number - C++

---
+++ C++/EASY/factorial.cpp
@@ -0,0 +1,25 @@
+//factorial of N number using recursion - C++
+#include <iostream>
+using namespace std;
+
+// This function calculates the factorial of a given number 'n'
+int factorial(int n){
+     // Base case: If n is 1, factorial of 1 is 1
+    if(n == 1){
+        return n;
+    }
+    // Recursive case: Multiply n by the factorial of (n-1)
+   return n* factorial(n-1);
+}
+int main() {
+    
+    // Declare and initialize variables
+    int n = 5;       // User input: calculate factorial of 5
+    int res = 1;     // Store the result of the factorial
+    
+    res=factorial(n); // Call the factorial function to calculate the factorial of 'n'
+    
+    cout<<"Factorial of "<<n<<":"<<res;
+
+    return 0;
+}
